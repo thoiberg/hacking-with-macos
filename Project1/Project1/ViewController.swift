1c326bfc3ca67b7404bf1460c70953a9516d7ff0
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-09-22 00:58:25 +1000

    Complete Project 1: Storm Viewer

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Timothy Hoiberg on 21/9/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
