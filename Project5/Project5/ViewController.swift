ba66a5f2a098cb8f8defc055568c466dfb70e240
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-09-22 15:35:47 +1000

    Add initial setup

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Timothy Hoiberg on 22/9/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
