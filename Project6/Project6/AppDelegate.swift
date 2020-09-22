b7ede48b284e141ee29abbccea7bb99446802b0c
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-09-22 17:29:18 +1000

    Complete project 6

---
+++ Project6/Project6/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Project6
+//
+//  Created by Timothy Hoiberg on 22/9/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
