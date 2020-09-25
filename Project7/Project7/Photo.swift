58b5d36124dbded1207a5431c97516ebf2664d7b
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-09-26 00:44:21 +1000

    Complete Project 7

--- Project7/Project7/Photo.swift
+++ Project7/Project7/Photo.swift
@@ -8,10 +8,36 @@
 import Cocoa
 
 class Photo: NSCollectionViewItem {
+    
+    let selectedBorderThickness: CGFloat = 3
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        view.wantsLayer = true
+        view.layer?.borderColor = NSColor.blue.cgColor
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                view.layer?.borderWidth = selectedBorderThickness
+            } else {
+                view.layer?.borderWidth = 0
+            }
+        }
+    }
+    
+    override var highlightState: NSCollectionViewItem.HighlightState {
+        didSet {
+            if highlightState == .forSelection {
+                view.layer?.borderWidth = selectedBorderThickness
+            } else {
+                if !isSelected {
+                    view.layer?.borderWidth = 0
+                }
+            }
+        }
     }
     
 }
