0efc19c1ae32c745738a356e8734d84d3902a615
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-09-29 22:59:05 +1000

    Initial setup

---
+++ Project11/Project11/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Project11
+//
+//  Created by Timothy Hoiberg on 29/9/20.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
