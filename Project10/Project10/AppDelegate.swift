258a1228ad54c46bdd6e86899bf3f6b1b9398ca5
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-09-29 22:51:45 +1000

    Not working

--- Project10/Project10/AppDelegate.swift
+++ Project10/Project10/AppDelegate.swift
@@ -9,18 +9,33 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        statusItem.button?.title = "Fetching..."
+        statusItem.menu = NSMenu()
+        addConfigurationMenuItem()
+        print("meow meow meow")
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func addConfigurationMenuItem() {
+        let separator = NSMenuItem(title: "Settings", action: #selector(showSettings), keyEquivalent: "")
+        statusItem.menu?.addItem(separator)
+    }
+    
+    @objc func showSettings(_ sender: NSMenuItem) {
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else { return }
+        
+        let popoverView = NSPopover()
+        popoverView.contentViewController = vc
+        popoverView.behavior = .transient
+        popoverView.show(relativeTo: statusItem.button!.bounds, of: statusItem.button!, preferredEdge: .maxY)
+    }
 }
 
--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -6,21 +6,20 @@
 //
 
 import Cocoa
+import MapKit
 
 class ViewController: NSViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var apiKey: NSTextField!
+    @IBOutlet var statusBarOption: NSPopUpButton!
+    @IBOutlet var units: NSSegmentedControl!
+    @IBOutlet var showPoweredBy: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
