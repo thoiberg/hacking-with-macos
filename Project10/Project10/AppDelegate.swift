6cc4a339cd576bdc3928ee698f4f3d6b08f87c80
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-09-29 22:03:35 +1000

    Initial setup

---
+++ Project10/Project10/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project10
+//
+//  Created by Timothy Hoiberg on 29/9/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Timothy Hoiberg on 29/9/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
