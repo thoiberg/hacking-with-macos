1444d41a5fd714e6ced9baa74b8dc654901bc949
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-12-25 17:15:21 +1000

    Initial commit for Project-12

---
+++ Project12/Project12/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project12
+//
+//  Created by Timothy Hoiberg on 25/12/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project12/Project12/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project12
+//
+//  Created by Timothy Hoiberg on 25/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
