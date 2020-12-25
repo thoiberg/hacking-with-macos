50d576df181be1d9e9f388ad576ee9a3dee1a016
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-12-25 19:11:49 +1000

    Add canvas elements

---
+++ Project13/Project13/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Project13
+//
+//  Created by Timothy Hoiberg on 25/12/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Timothy Hoiberg on 25/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
