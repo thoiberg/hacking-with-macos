ebcb333c014e442a31585d5d3fc9386233be6bc7
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-12-30 20:03:55 +1100

    Set up Project 14

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Timothy Hoiberg on 30/12/20.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
