a3b5142a5d2f668b52db462ac5d78f60ea8c6ac9
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-12-31 20:56:47 +1100

    Initial setup

---
+++ Project17/Project17/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Project17
+//
+//  Created by Timothy Hoiberg on 31/12/20.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
+    }
+    
+    
+}
---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Timothy Hoiberg on 31/12/20.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+   override func didMove(to view: SKView) {
+   }
+}
