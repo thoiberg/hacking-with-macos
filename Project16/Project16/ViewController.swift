f5f3c4f1806ce51a30461b1d6551ef282cfdc55f
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-12-31 15:58:51 +1100

    Initial setup

---
+++ Project16/Project16/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Timothy Hoiberg on 31/12/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
